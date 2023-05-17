5db32e8766df81cbcb32df2f7451a7d379ea9ebd
Ricardo Melo Martins <coreexpress@gmail.br>
2023-05-17 15:47:09 -0300

    first server poc release

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# dedilly
+# Dedilly test
+
+## Servidor
+
+Para rodar a primeira versão do servidor
+
+```bash
+$ cd \server\poc 
+```
+
+rodar o comando
+
+```bash
+$ npm start
+```
+
+Para testar acessar a Url fornecendo um endereço
+
+```
+http://localhost:3000?q={endereço}
+```
+
+exemplo
+
+```
+http://localhost:3000?q=Av%20Paulista
+```
+
+O resultado deverá ser um json parecido com este
+```
+{
+	"latitude": -23.75,
+	"longitude": -46.625,
+	"temperature": 17.9,
+	"windspeed": 12,
+	"city": "São Paulo, Brasil"
+}
+
+```
+
+
+## Cliente
+
+Para rodar a versão cliente
+
+
+```bash
+$ cd \client\poc 
+```
+
+rodar o comando
+
+```bash
+$ npm start
+```
+
---
+++ server/poc/index.js
@@ -0,0 +1,77 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from "axios";
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(bodyParser.json())
+
+
+app.get('/', async (req, res, next) => {
+
+    let address, lat, long, displayName, currentWeather
+
+    address = req.query.q;
+
+    if (!address && !address.length) {
+        res.status(404).json({ message: "Localidade não fornecida!" });
+    }
+
+    let urlApiGeocode = `https://geocode.maps.co/search?q=${address}`;
+
+    try {
+        
+        const resApi = await axios(urlApiGeocode);
+        let resApiData;
+
+        if (resApi && resApi.data.length > 0) {
+            resApiData = resApi.data
+            
+            // res.status(200).json(resApiData);
+
+            lat = resApiData[0].lat;
+            long = resApiData[0].lon;
+
+            displayName = resApiData[0].display_name
+
+
+            if (!lat || !long) {
+                res.status(404).json({ message: "Localidade não encontrada!" });
+            }
+            
+            currentWeather = 'true';
+        
+            let urlApiWeather = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current_weather=${currentWeather}`;
+    
+            try {
+                const receivedWeatherData = await axios(urlApiWeather); 
+                
+                let response = {};
+
+                    response = {
+                        latitude: receivedWeatherData.data.latitude,
+                        longitude: receivedWeatherData.data.longitude,
+                        temperature: receivedWeatherData.data.current_weather.temperature,
+                        windspeed: receivedWeatherData.data.current_weather.windspeed,
+                        city: displayName
+                    }
+                
+                res.status(200).json(response);
+
+            } catch (error) {
+                next(error);
+            }
+        
+        }
+        
+
+    } catch (error) {
+        next(error);
+    }   
+
+  });
+
+app.listen(3000, () => {
+  console.log('escutando...');
+});
